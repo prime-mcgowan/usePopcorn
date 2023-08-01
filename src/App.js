16c08b72379993ee21c4a1c6c1b58aa02e4f125d
Laura McGowan <prime.lmcg@gmail.com>
2023-08-01 16:04:28 -0500

    added a Loader component & state = users will now see Loading... when the application is indeed loading the data

--- src/App.js
+++ src/App.js
@@ -56,16 +56,19 @@ const KEY = "fafaa94b";
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const query = "star wars";
 
   useEffect(function () {
     async function fetchMovies() {
+      setIsLoading(true);
       const res = await fetch(
         `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
       );
       const data = await res.json();
       setMovies(data.Search);
       console.log(data.Search);
+      setIsLoading(false);
     }
     fetchMovies();
   }, []);
@@ -79,9 +82,7 @@ export default function App() {
       </NavBar>
 
       <Main>
-        <Box>
-          <MovieList movies={movies} />
-        </Box>
+        <Box>{isLoading ? <Loader /> : <MovieList movies={movies} />}</Box>
 
         <Box>
           <WatchedSummary watched={watched} />
@@ -90,6 +91,10 @@ export default function App() {
       </Main>
     </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>;
 }
 
 // * NavBar Component

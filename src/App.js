1ca320c3a043c3b0d00d6d24f9b6f58154e10f91
Laura McGowan <prime.lmcg@gmail.com>
2023-08-01 10:29:23 -0500

    used the {children} prop and moved: Search, NumResults, ListBox, MovieList and WatchedBox into the App component so {movie} did not need to be needlessly passed down

--- src/App.js
+++ src/App.js
@@ -56,19 +56,28 @@ export default function App() {
 
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        {/* Children of the NavBar */}
+        <Search />
+        <NumResults movies={movies} />
+      </NavBar>
+
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
 // * NavBar Component
-function NavBar({ movies }) {
+function NavBar({ children }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <NumResults movies={movies} />
+      {children}
     </nav>
   );
 }
@@ -105,17 +114,12 @@ function NumResults({ movies }) {
 }
 
 // * Main Component
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
 // * ListBox Component (left side)
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -126,7 +130,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }

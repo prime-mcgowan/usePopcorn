b7664170f583d54590d6cd0a737accd11b446bc6
Laura McGowan <prime.lmcg@gmail.com>
2023-08-04 12:51:35 -0500

    used .toFixed(1) on avgImdbRating and avgUserRating...these numbers will now only go out 1 decimal point

--- src/App.js
+++ src/App.js
@@ -422,11 +422,11 @@ function WatchedSummary({ watched }) {
         </p>
         <p>
           <span>⭐️</span>
-          <span>{avgImdbRating}</span>
+          <span>{avgImdbRating.toFixed(1)}</span>
         </p>
         <p>
           <span>🌟</span>
-          <span>{avgUserRating}</span>
+          <span>{avgUserRating.toFixed(1)}</span>
         </p>
         <p>
           <span>⏳</span>

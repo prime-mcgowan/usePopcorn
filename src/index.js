711c3bc05b1b06874202bd9e6d15233475cfcc0f
Laura McGowan <prime.lmcg@gmail.com>
2023-08-17 09:19:19 -0500

    only 3 movies listed w/a scrollbar when @media 767

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "../src/responsive.css";
 import App from "./App";
 
 // import StarRating from "./StarRating";
